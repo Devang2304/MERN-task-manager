20b7d3da886c77eeeb3f2568b22f815036b775d3
Devang2304 <devangvartak2304@gmail.com>
2022-11-19 23:46:52 +0530

    edited file structure

---
+++ task-manager/src/layouts/MainLayout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const MainLayout=({children})=>{
+    return (
+        <>
+        <div className="relative bg-gray-50 h-screen w-screen overflow-x-hidden">
+            <Navbar/>
+        </div>
+        </>
+    )
+}
+
+export default MainLayout;
